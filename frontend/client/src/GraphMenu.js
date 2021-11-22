325bf43d5399bfa66863e1bb0fac51fef43fda0f
andrewtran10 <andtran@umass.edu>
2021-11-21 21:17:35 -0500

    added graph menu component

---
+++ frontend/client/src/GraphMenu.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles'
+import Box from '@mui/material/Box';
+
+
+const useStyles = makeStyles(theme => ({
+    containter: {
+        backgroundColor: "#000000"
+    },
+    marginAutoItem: {
+      display: "inline",
+      textAlign: "center"
+    },
+    graph: {
+        margin: "auto"
+    }
+  }))
