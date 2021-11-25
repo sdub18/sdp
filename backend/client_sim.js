9bffea49f61e9255b7f2cf4e54d5e6471d0c49fe
andrewtran10 <andtran@umass.edu>
2021-11-25 01:15:21 -0500

    update client sim to include port in init packet

--- backend/client_sim.js
+++ backend/client_sim.js
@@ -4,16 +4,17 @@ const { exit } = require('process');
 //change HOST to your PC's ip address
 const HOST = "localhost";
 const PORT = 49160;
+const UID = process.argv[2];
 
 const options = {family: 4, host:HOST, port: PORT}
 const client = net.createConnection(options, connectionHandler);
-
+let canSend = false;
 
 function connectionHandler(conn){
     c_addr = client.address();
     console.log("established new connection using %s:%s", c_addr.address, c_addr.port);
 
-    sendData();
+    sendData(c_addr.port);
 
     client.on('data', (d)=>{
         console.log(''+d);
@@ -29,26 +30,35 @@ function connectionHandler(conn){
 }
 
 
-async function sendData() {
+async function sendData(port) {
     while(1){
-        let I = 100 + getRandomIntInRange(-10, 10);
-        let V = 11 + (getRandomIntInRange(0,10)/10);
-        let Ta = 60 + getRandomIntInRange(0,5);
-        let Tc = 80 + getRandomIntInRange(0,5);
-        let x = 0;
-        let y = 9.8;
-        let z = 0;
-        
         let header = String.fromCharCode(1);
         let end = String.fromCharCode(0);
-        let data = `{"current": ${I}, "voltage": ${V}, "temp_ambient": ${Ta}, "temp_casing": ${Tc}, "accelereation": {"x": ${x}, "y": ${y}, "z": ${z}}}`;
-        let data_pkt = `{"port": ${client.address().port}, "data":${data}}`;
-        data_pkt = header + data_pkt + end;
+        let data, type = "";
+
+        if (!canSend){
+            type = "init";
+            data = port;
+        }
+        else{
+            let I = 100 + getRandomIntInRange(-10, 10);
+            let V = 11 + (getRandomIntInRange(0,10)/10);
+            let Ta = 60 + getRandomIntInRange(0,5);
+            let Tc = 80 + getRandomIntInRange(0,5);
+            let x = 0;
+            let y = 9.8;
+            let z = 0;
+            type = "data";
+            data = `{"current": ${I}, "voltage": ${V}, "temp_ambient": ${Ta}, "temp_casing": ${Tc}, "accelereation": {"x": ${x}, "y": ${y}, "z": ${z}}}`;
+        }
+
+        let load = `{"id": ${UID}, "data":${data}}`;  
+        data_pkt = header + load + end;
+            
+        client.emit('data',data_pkt);
+        console.log(data_pkt);
         
-        client.write(data_pkt);
-        
-        await sleep(1);
-    
+        await sleep(1000);  
     }    
 }
 
