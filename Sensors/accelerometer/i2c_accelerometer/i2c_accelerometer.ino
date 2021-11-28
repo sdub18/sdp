2e7066b94fe17955a0756ad3eeec1a3c2d901ef5
sdub18 <sdubois@umass.edu>
2021-11-28 18:02:42 -0500

    Added accelerometer isolated test

--- Sensors/accelerometer/i2c_accelerometer/i2c_accelerometer.ino
+++ Sensors/accelerometer/i2c_accelerometer/i2c_accelerometer.ino
@@ -1,38 +1,19 @@
-
-#include <Wire.h>
-#include "Adafruit_MCP9808.h"
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_Sensor.h>
-#include <SPI.h>
-// Used for software SPI
-#define LIS3DH_CLK 13
-#define LIS3DH_MISO 12
-#define LIS3DH_MOSI 11
-// Used for hardware & software SPI
-#define LIS3DH_CS 10
 
-// software SPI
-//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS, LIS3DH_MOSI, LIS3DH_MISO, LIS3DH_CLK);
-// hardware SPI
-//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS);
-// I2C
 Adafruit_LIS3DH lis = Adafruit_LIS3DH();
 
-Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
 
   Serial.println("LIS3DH test!");
 
-  if (! lis.begin(0x1A)) {   // change this to 0x19 for alternative i2c address
+  if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
     Serial.println("Couldnt start");
     while (1) yield();
   }
   Serial.println("LIS3DH found!");
-
-  // lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
 
   Serial.print("Range = "); Serial.print(2 << lis.getRange());
   Serial.println("G");
@@ -53,24 +34,9 @@ void setup() {
     case LIS3DH_DATARATE_LOWPOWER_1K6HZ: Serial.println("16 Khz Low Power"); break;
   }
 
-  if (!tempsensor.begin(0x19)) {
-    Serial.println("Couldn't find MCP9808! Check your connections and verify the address is correct.");
-    while (1);
-  }
-    
-   Serial.println("Found MCP9808!");
-
-  tempsensor.setResolution(3); // sets the resolution mode of reading, the modes are defined in the table bellow:
-  // Mode Resolution SampleTime
-  //  0    0.5°C       30 ms
-  //  1    0.25°C      65 ms
-  //  2    0.125°C     130 ms
-  //  3    0.0625°C    250 ms
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   lis.read();      // get X Y and Z data at once
   // Then print out the raw data
   Serial.print("X:  "); Serial.print(lis.x);
@@ -90,20 +56,4 @@ void loop() {
   Serial.println();
 
   delay(100);
- // wake up MCP9808 - power consumption ~200 mikro Ampere
-  tempsensor.wake();   // wake up, ready to read!
-
-  // Read and print out the temperature, also shows the resolution mode used for reading.
-  float c = tempsensor.readTempC();
-  float f = tempsensor.readTempF();
-  Serial.print("Temp: "); 
-  Serial.print(c, 4); Serial.print("*C\t and "); 
-  Serial.print(f, 4); Serial.println("*F.");
-  
-  delay(50);
-  tempsensor.shutdown_wake(1); // shutdown MSP9808 - power consumption ~0.1 mikro Ampere, stops temperature sampling
-  Serial.println("");
-  delay(100);
-  
-
 }
--- Sensors/combined_sensors/temp_accel_curr/temp_accel_curr.ino
+++ Sensors/combined_sensors/temp_accel_curr/temp_accel_curr.ino
@@ -3,6 +3,8 @@
 #include "Adafruit_MCP9808.h"
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_Sensor.h>
+#include <Adafruit_INA260.h>
+
 #include <SPI.h>
 // Used for software SPI
 #define LIS3DH_CLK 13
@@ -17,41 +19,46 @@
 //Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS);
 // I2C
 Adafruit_LIS3DH lis = Adafruit_LIS3DH();
-
 Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
+Adafruit_INA260 ina260 = Adafruit_INA260();
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
 
-  Serial.println("LIS3DH test!");
+  // MARK: ACCELEROMETER SENSOR
 
-  if (! lis.begin(0x1A)) {   // change this to 0x19 for alternative i2c address
-    Serial.println("Couldnt start");
-    while (1) yield();
-  }
-  Serial.println("LIS3DH found!");
+//  Serial.println("LIS3DH test!");
+//
+//  if (! lis.begin(0x1A)) {   // change this to 0x19 for alternative i2c address
+//    Serial.println("Couldnt start");
+//    while (1) yield();
+//  }
+//  Serial.println("LIS3DH found!");
+//
+//  // lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
+//
+//  Serial.print("Range = "); Serial.print(2 << lis.getRange());
+//  Serial.println("G");
+//
+//  // lis.setDataRate(LIS3DH_DATARATE_50_HZ);
+//  Serial.print("Data rate set to: ");
+//  switch (lis.getDataRate()) {
+//    case LIS3DH_DATARATE_1_HZ: Serial.println("1 Hz"); break;
+//    case LIS3DH_DATARATE_10_HZ: Serial.println("10 Hz"); break;
+//    case LIS3DH_DATARATE_25_HZ: Serial.println("25 Hz"); break;
+//    case LIS3DH_DATARATE_50_HZ: Serial.println("50 Hz"); break;
+//    case LIS3DH_DATARATE_100_HZ: Serial.println("100 Hz"); break;
+//    case LIS3DH_DATARATE_200_HZ: Serial.println("200 Hz"); break;
+//    case LIS3DH_DATARATE_400_HZ: Serial.println("400 Hz"); break;
+//
+//    case LIS3DH_DATARATE_POWERDOWN: Serial.println("Powered Down"); break;
+//    case LIS3DH_DATARATE_LOWPOWER_5KHZ: Serial.println("5 Khz Low Power"); break;
+//    case LIS3DH_DATARATE_LOWPOWER_1K6HZ: Serial.println("16 Khz Low Power"); break;
+//  }
 
-  // lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
-
-  Serial.print("Range = "); Serial.print(2 << lis.getRange());
-  Serial.println("G");
-
-  // lis.setDataRate(LIS3DH_DATARATE_50_HZ);
-  Serial.print("Data rate set to: ");
-  switch (lis.getDataRate()) {
-    case LIS3DH_DATARATE_1_HZ: Serial.println("1 Hz"); break;
-    case LIS3DH_DATARATE_10_HZ: Serial.println("10 Hz"); break;
-    case LIS3DH_DATARATE_25_HZ: Serial.println("25 Hz"); break;
-    case LIS3DH_DATARATE_50_HZ: Serial.println("50 Hz"); break;
-    case LIS3DH_DATARATE_100_HZ: Serial.println("100 Hz"); break;
-    case LIS3DH_DATARATE_200_HZ: Serial.println("200 Hz"); break;
-    case LIS3DH_DATARATE_400_HZ: Serial.println("400 Hz"); break;
-
-    case LIS3DH_DATARATE_POWERDOWN: Serial.println("Powered Down"); break;
-    case LIS3DH_DATARATE_LOWPOWER_5KHZ: Serial.println("5 Khz Low Power"); break;
-    case LIS3DH_DATARATE_LOWPOWER_1K6HZ: Serial.println("16 Khz Low Power"); break;
-  }
+  // MARK: TEMPERATURE SENSOR
 
   if (!tempsensor.begin(0x19)) {
     Serial.println("Couldn't find MCP9808! Check your connections and verify the address is correct.");
@@ -67,9 +74,22 @@ void setup() {
   //  2    0.125°C     130 ms
   //  3    0.0625°C    250 ms
 
+
+  // MARK: CURRENT SENSOR SETUP
+
+  Serial.println("Searching for Current Sensor ..."); // Search for INA260 Chip
+
+  if (!ina260.begin(0x18)) {
+    Serial.println("Couldn't find INA260 chip");
+    while (1);
+  }
+  Serial.println("Found INA260 chip");
 }
 
 void loop() {
+
+  // PRINT ACCELEROMETER SENSOR DATA
+  
   // put your main code here, to run repeatedly:
   lis.read();      // get X Y and Z data at once
   // Then print out the raw data
@@ -90,6 +110,9 @@ void loop() {
   Serial.println();
 
   delay(100);
+
+  // PRINT TEMPERATURE SENSOR DATA
+  
  // wake up MCP9808 - power consumption ~200 mikro Ampere
   tempsensor.wake();   // wake up, ready to read!
 
@@ -104,6 +127,23 @@ void loop() {
   tempsensor.shutdown_wake(1); // shutdown MSP9808 - power consumption ~0.1 mikro Ampere, stops temperature sampling
   Serial.println("");
   delay(100);
+
+  // PRINT CURRENT SENSOR DATA
+  
+  Serial.print("Current: ");
+  Serial.print(ina260.readCurrent());
+  Serial.println(" mA");
+
+  Serial.print("Bus Voltage: ");
+  Serial.print(ina260.readBusVoltage());
+  Serial.println(" mV");
+
+  Serial.print("Power: ");
+  Serial.print(ina260.readPower());
+  Serial.println(" mW");
+
+  Serial.println();
+  delay(1000);
   
 
 }
