1a6267d53b74711e4a23756e608f3c6123872061
andrewtran10 <andtran@umass.edu>
2021-11-29 01:55:44 -0500

    restructure code; comments

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -9,40 +9,39 @@ import AddonDropdown from "./AddonDropdown";
 const ChartButtonsMemo = React.memo(ChartButtons);
 const AddonDropdownMemo = React.memo(AddonDropdown);
 
-const RENDER_PERIOD = 50;
-
-const config = {"xMax" : 100, 
+let local_addons = [];          // frontend local copy of connected addons
+const RENDER_PERIOD = 50;       // rerender period in milliseconds
+const socket_options = {'reconnection': true, 'reconnectionAttempts': Infinity} // options to have frontend continuously try to reconnect to backend
+const socket = io('http://localhost:3001', socket_options);       // frontend websocket - connects to backend server's websocket
+const chart_types = ["current", "temp_ambient", "temp_casing"];   // all chart types --> HARDCODED AND KEPT IN FRONTEND; NOT STORED IN BACKEND
+const coordinates = [];               // frontend local copy of coordinates, used to set "coords" state variable
+const config = {"xMax" : 100,         // chart config --> consider moving to backend so we can keep multiple different copies depending on chart type
                 "xIncrement" : 100,
                 "yMin" : 0,
                 "yMax" : 200,
                 "width" : 700,
                 "height" : 400};
 
-// instantiate "client (frontend)" websocket connected to M2F server on port 3001"
-const socket = io('http://localhost:3001', {'reconnection': true, 'reconnectionAttemps': Infinity});
+for (let i = 0; i < config.xMax; i++)   // instantiate coordinates in array
+  coordinates.push({x: i, y: 0});
 
-// instanstiate coordinates array
-const coordinates = [];
-for (let i = 0; i < config.xMax; i++) coordinates.push({x: i, y: 0})
-
-// update coordinates array with incoming data_pt
+// update coordinates upon receiving new data point from backend, shift y coordinates back by 1 position
 socket.on('data', (data_pt) => {
-  for (let i = 0; i < coordinates.length-1; i++) coordinates[i].y = coordinates[i+1].y;
+  for (let i = 0; i < coordinates.length-1; i++) 
+    coordinates[i].y = coordinates[i+1].y;
+
   coordinates[coordinates.length-1].y = data_pt;
 });
 
-// update received addons array
-let local_addons = [];
+// update local copy of list of addon ids upon receiving different list than current local copy
 socket.on("updateAddons", (recv_addons) => {
-  if (!(JSON.stringify(recv_addons) === JSON.stringify(local_addons))){
+  if (!(JSON.stringify(recv_addons) === JSON.stringify(local_addons)))
     local_addons = recv_addons; 
-  }
 });
 
-const chart_types = ["current", "temp_ambient", "temp_casing"];
 
 function App() {
-  const [chartType, setChartType] = React.useState("");
+  const [chartType, setChartType] = React.useState("");  
   const [thing, setThing] = React.useState(0);
   const [coords, setCoords] = React.useState([]);
   const [addons, setAddons] = React.useState([]);
@@ -50,6 +49,7 @@ function App() {
 
 
   React.useEffect(() => {
+    // update coords and addons state variable every rerender period to avoid lag
     const timer = setInterval(() => {
       if (local_addons.toString() !== addons) setAddons(local_addons);
       setThing(coordinates[coordinates.length-1].y);
@@ -67,8 +67,8 @@ function App() {
   const chooseAddon = React.useCallback((event) => {
     const addon = event.target.value
     socket.emit("addon_selection", addon);
-    // reset coordinates when new addon selected
-    for (let i = 0; i < config.xMax; i++) coordinates[i].y = 0;
+    for (let i = 0; i < config.xMax; i++) 
+      coordinates[i].y = 0;
     setSelectedAddon(addon);
   }, []);
 
@@ -79,7 +79,7 @@ function App() {
         <AddonDropdownMemo labels={addons} value={selectedAddon} onChangeHandler={chooseAddon}/>
         <br/>
         <ChartButtonsMemo labels={chart_types} onChangeHandler={chooseChartType}/>
-        {chartType !== "" &&
+        {chartType !== "" && selectedAddon !== "" &&
           <DynamicGraph
                 title={chartType}
                 data={coords}
