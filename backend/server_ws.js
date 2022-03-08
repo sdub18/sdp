b39ed3b5a3c3888ca9532c062d8b254b0f317f46
Michael Brauninger <mbrauninger@umass.edu>
2022-03-08 15:20:25 -0500

    Update to get everything on-track for moving frontend computation to backend

--- backend/server_ws.js
+++ backend/server_ws.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const { config } = require("process");
 
 const CLIENT_TO_MIDDLE_PORT = process.env.PORT || 49160;
 const MIDDLE_TO_FRONT_PORT = process.env.PORT || 3001;
@@ -14,6 +15,14 @@ let selectedID = 0;   // select addon id to choose which data packet to access
 let val2emit = 0;     // backend local variable to update to send to frontend
 const data = [];      // hold separated data packets (one pkt for each addon) in list
 
+// -----------------------------------------------------------------------------------------------------------------------
+
+const unit_dict = {"current": "A", "power": "W", "temperature": "F", "rpm": "RPM"};
+const graph_labels = Object.keys(unit_dict);
+let graph_settings = null;
+let active_pid = null;
+let coordinates = {};
+
 
 M2F_socket.on("connection", M2F_connectionHandler);
 M2F_server.listen(MIDDLE_TO_FRONT_PORT, () => {console.log(`C2M_server listening on ${MIDDLE_TO_FRONT_PORT}`)});
@@ -21,31 +30,40 @@ M2F_server.listen(MIDDLE_TO_FRONT_PORT, () => {console.log(`C2M_server listening
 C2M_server.on('connection', C2M_connectionHandler);
 C2M_server.listen({host: "0.0.0.0", port:CLIENT_TO_MIDDLE_PORT}, () => console.log('opened C2M_server on ', C2M_server.address()));
 
-// First line is the reception of what PID's graphs should be displayed. This value is stored in selectedID
-// Function then ensures that the val2emit is not null, which should be the case after at least one process has connected
-// to the server, and the user has selected a process to be viewed. After doing this, the server sends the full JSON
-// associated with that process to the frontend.
-// Function also updates the array of active PIDs and communicates that to the frontend.
-function M2F_connectionHandler(client){
-  client.on("addon_selection", (arg) => {selectedID = arg});
-  
-  // separate the interval processes to emit the data and to select the data
-  setInterval(() => {
-    if (val2emit.length > 0 && val2emit !== 0) {
-      if (val2emit[0].id === selectedID) {
-        client.emit('data', val2emit);
+/*
+This is the first thing that runs in the entire project infrastructure. It begins by asking the frontend for the parameters that are
+being used in the graphs, so the backend can store/modify data correctly and send it to the frontend. Once the response has been
+received, the JSON coordinated object is populated based on the info contained within the response.
+*/
+function createEmptyGraph(graph_settings) {
+  labels_coords_dict = {};
+    for (let i = 0; i < graph_labels.length; i++) {
+      labels_coords_dict[graph_labels[i]] = [];
+      for (let j = 0; j < graph_settings.xMax; j++) {
+        labels_coords_dict[graph_labels[i]].push({x: j, y: 0, threshold: 50});
       }
     }
-  }, EMIT_PERIOD);     
-  setInterval(() => {
-    client.emit("updateAddons", addons.map(a => a.id));  
-    val2emit = data;
-  }, 1);
+    return labels_coords_dict;
 }
 
+function M2F_connectionHandler(client){
+  client.emit("config");
+
+  client.on("config_response", (info) => {
+    console.log("connected to frontend");
+    graph_settings = info;
+  })
+  
+  client.on("addon_selection", (pid) => {
+    console.log(pid);
+    active_pid = pid;
+  })
+}
+
+/*
 // When the initial connection is made with the client, 'new client connection from %s' should be printed
 // The function should parse the data, where parseData(recv_d, data) places the data in a JSON that looks something like
-/*
+
 [
   {
     id: 50043,
@@ -68,25 +86,47 @@ function C2M_connectionHandler(conn){
   });
 
   conn.on('error', (err) => {console.log('Connection %s error: %s', remoteAddress, err.message)});
+
   conn.on('data', (recv_d) => {
     parseData(recv_d, data)     // parse buffer stream into individual packets of data and place into data array
     for (let pkt of data) { 
       // update local addon array if new addon detected and write back to addon to start sending sensor data
       if (!addons.some(addon => addon.id === pkt.id)) {
         addons.push(pkt);
-        conn.write(Buffer.from([0x01]));
-        console.log(addons);
+        coordinates[pkt.id] = createEmptyGraph(graph_settings);
+        conn.write("nice");
+        M2F_socket.emit("updateAddons", addons.map(a => a.id));
+        console.log(addons.map(a => a.id));
+      }
+      for (let i = 0; i < graph_labels.length; i++) {
+        for (let j = 0; j < graph_settings.xMax - 1; j++) {
+          coordinates[pkt.id][graph_labels[i]][j].y = coordinates[pkt.id][graph_labels[i]][j+1].y;
+        }
+        coordinates[pkt.id][graph_labels[i]][graph_settings.xMax - 1].y = pkt.data[graph_labels[i]];
+      }
+      if (pkt.id === active_pid) {
+        current_coords = coordinates[active_pid];
+        sending_coords_array = [];
+        for (let i = 0; i < graph_labels.length; i++) {
+          sending_coords_array.push(current_coords[graph_labels[i]]);
+        }
+        M2F_socket.emit("graph_update", sending_coords_array);
       }
     }
   });
+
   conn.on('close', () => {
     // remove connection from addon array
     console.log('connection from %s closed', conn.remotePort);
-    addon_idx = addons.findIndex(addon => addon.data == conn.remotePort);
-    addons.splice(addon_idx, 1);
-    console.log(addons);
+    addon_index = addons.findIndex(addon => addon.data == conn.remotePort);
+    addon_id = addons[addon_index].id;
+    addons.splice(addon_index, 1);
+    delete coordinates[addon_id];
+    M2F_socket.emit("updateAddons", addons.map(a => a.id));
+    console.log(addons.map(a => a.id));
   });
 }
+
 
 // See the description for C2M_connectionHandler()
 function parseData(recv_data, pkts_array){
