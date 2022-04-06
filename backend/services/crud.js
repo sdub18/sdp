a9a279074680be8f723796e01cc6e0b280a938e7
andrewtran10 <andtran@umass.edu>
2022-04-06 07:24:37 -0400

    include all data packet values in db insert

--- backend/services/crud.js
+++ backend/services/crud.js
@@ -1,20 +1,19 @@
 const db = require('../db');
 
 function insertData(pkt) {
-	const id = pkt['id'];
 	const data = pkt['data'];
 
+	const timestamp = Date.now();
+	const id = pkt['id'];
 	const current = data['current'];
 	const voltage = data['voltage'];
 	const power = data['power'];
 	const temp = data['temp'];
-	const accelereation = data['acceleration'];
+	const a = data['acceleration'];
 
-	const timestamp = Date.now();
+	const sqlCmd = 'INSERT INTO data VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
 	
-	const sqlCmd = 'INSERT INTO data VALUES (?, ?, ?, ?, ?)';
-	
-	db.insert(sqlCmd, [id, timestamp, current, temp, power]);
+	db.insert(sqlCmd, [timestamp, id, current, voltage, power, temp, a['x'], a['y'], a['z']]);
 };
 
 function getData(){
