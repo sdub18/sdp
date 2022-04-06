5cf9165e6f8e8ff8fac5f232860c9f7339fac8a6
andrewtran10 <andtran@umass.edu>
2022-04-06 02:03:25 -0400

    delete+recreate db file on startup

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,9 @@
 const DBSOURCE = "db.sqlite";
 const Database = require('better-sqlite3');
+const fs = require('fs');
 
-require('fs').unlinkSync(DBSOURCE)
+if (fs.existsSync(DBSOURCE)) fs.unlinkSync(DBSOURCE);
+
 const db = new Database(DBSOURCE, { verbose: console.log });
 db.exec("CREATE TABLE data (timestamp DATE NOT NULL, id INTEGER NOT NULL, current REAL, temp REAL, power REAL)");
 
