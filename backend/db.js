92d2d87eb1302a35a9879b24a12478ec314992c9
andrewtran10 <andtran@umass.edu>
2022-04-06 01:55:28 -0400

    INSERT functionality into DB

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,9 @@
-const Database = require('better-sqlite3');
 const DBSOURCE = "db.sqlite";
+const Database = require('better-sqlite3');
 
+require('fs').unlinkSync(DBSOURCE)
 const db = new Database(DBSOURCE, { verbose: console.log });
+db.exec("CREATE TABLE data (timestamp DATE NOT NULL, id INTEGER NOT NULL, current REAL, temp REAL, power REAL)");
 
 function query(sql, params) {
 	return db.prepare(sql).all(params);
--- backend/services/crud.js
+++ backend/services/crud.js
@@ -1,7 +1,20 @@
 const db = require('../db');
 
-function insertData(data_pkt) {
-	console.log(data_pkt);
+function insertData(pkt) {
+	const id = pkt['id'];
+	const data = pkt['data'];
+
+	const current = data['current'];
+	const voltage = data['voltage'];
+	const power = data['power'];
+	const temp = data['temp'];
+	const accelereation = data['acceleration'];
+
+	const timestamp = Date.now();
+	
+	const sql = 'INSERT INTO data VALUES (?, ?, ?, ?, ?)';
+	
+	db.insert(sql, [id, timestamp, current, temp, power]);
 };
 
 
