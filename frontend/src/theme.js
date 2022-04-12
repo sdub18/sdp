fef7a712e299d12d010cb90ba20bb963e2647f02
andrewtran10 <andtran@umass.edu>
2022-04-12 10:47:54 -0400

    theme formatting

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,13 +1,12 @@
 import { createTheme } from "@material-ui/core";
-import { amber } from "@material-ui/core/colors";
 
 export const theme = createTheme({
 	palette: {
 		background: {
-		  default: "#32363d"
+		  	default: "#32363d"
 		},
 		text: {
-		  primary: "#ffffff"
+		  	primary: "#ffffff"
 		}
 	  }
 })
