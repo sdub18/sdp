5aa44e85e5260555d48c6b4eb72a17cd8bd4fe1c
andrewtran10 <andtran@umass.edu>
2022-04-18 11:25:21 -0400

    Fix merge fuckup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,51 +11,6 @@ import AppContainer from "./containers/AppContainer";
 
 
 function App() {
-  const [coords, setCoords] = React.useState([]);
-  const [addons, setAddons] = React.useState([]);
-  const [selectedAddon, setSelectedAddon] = React.useState("");
-  const [selectedPeriod, setSelectedPeriod] = React.useState("");
-  const [config, setConfig] = React.useState({"xMax" : 300,         
-  "xIncrement" : 100,
-  "width" : 700,
-  "height" : 400,
-});
-  const [processDict_App, setProcessDict] = React.useState({});
-  const [threshold, setThreshold] = React.useState(50);
-
-  React.useEffect(() => {
-    const timer = setInterval(() => {
-      setAddons(local_addons);
-      setCoords(coordinates);
-      setProcessDict(processDict);
-
-    }, RENDER_PERIOD);
-    return () => clearInterval(timer);
-  }, []);
-
-  React.useEffect(() => {
-    if (!(addons.includes(selectedAddon))){
-      setSelectedAddon("");
-    }
-  }, [addons])
-
-  const chooseAddon = React.useCallback((event) => {
-    const addon = event.target.value
-    socket.emit("addon_selection", addon);
-    setSelectedAddon(Number(addon));
-  }, []);
-
-  const choosePeriod = React.useCallback((event) => {
-    const period = event.target.value
-    let periodAndFrequency = {};
-    periodAndFrequency[period] = period_to_frequency[period];
-    socket.emit("period_selection", periodAndFrequency);
-    setSelectedPeriod(period);
-    let tempConfig = globalConfig;
-    tempConfig.xMax = period_to_xMax[period];
-    tempConfig.xIncrement = period_to_xIncrement[period];
-  }, []);
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline/>
