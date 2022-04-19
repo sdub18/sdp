4ff55047c0a7a167555159d34acf6ea9efdfc232
andrewtran10 <andtran@umass.edu>
2022-04-19 02:58:02 -0400

    update data parsing

--- backend/utils/parseData.js
+++ backend/utils/parseData.js
@@ -1,34 +1,18 @@
 function parseData(recv_data){
-	const parens = {"{": "}", "(": ")", "[": "]"};
-	const stack = []
 	let pkt_array = []
-	let pkt = "";
-  
-	try {
-	  pkt = JSON.parse(recv_data.toString('utf-8'));
-	  pkt_array.push(pkt);
-	}
-	catch (err) {
-	  for (let char in recv_data){
-		pkt += char;
-		if (parens.hasOwnProperty(char)) {
-		  stack.push(char)  
-		}
-		else if (Object.values(parens).some(p => p === char)){
-		  if (parens[stack.pop()] === char && stack.length === 0){
-			pkt = JSON.parse(pkt);
+	
+	recv_packets = recv_data.toString('utf-8').split("\n");
+	
+	recv_packets.forEach(split => {
+		try {
+			let pkt = JSON.parse(split);
 			pkt_array.push(pkt);
-			pkt = "";
 		  }
-		  else {
-			pkt_array.length = 0;
-			console.log("Failed to parse data");
-			break;
-		  }
-		}
-		else continue;
-	  }
-	}
+		  catch (err) {
+			
+		  }	
+	});
+	
 	return pkt_array;
   }
 
