0785e76bd1fc1edc18729157dd24a5574445f957
andrewtran10 <andtran@umass.edu>
2022-04-19 01:18:38 -0400

    Fix column headers on policy table

--- backend/db.js
+++ backend/db.js
@@ -8,7 +8,7 @@ const options = { verbose: console.log };
 const db = new Database(DBSOURCE);
 db.exec("CREATE TABLE IF NOT EXISTS data (timestamp DATE NOT NULL, id INTEGER NOT NULL, current REAL, voltage REAL, power REAL, temp REAL, x REAL, y REAL, z REAL)");
 db.exec("CREATE TABLE IF NOT EXISTS freq (fx REAL, fy REAL, fz REAL)");
-db.exec("CREATE TABLE IF NOT EXISTS policy (moduleId REAL, policyID REAL, policyType TEXT, dataType TEXT, period REAL, comparison TEXT, treshold REAL)");
+db.exec("CREATE TABLE IF NOT EXISTS policy (moduleID REAL, policyID REAL, policyType TEXT, dataType TEXT, period REAL, comparison TEXT, threshold REAL)");
 db.pragma("synchronous = off");
 
 function query(sql, params) {
