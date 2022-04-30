2bb8c89520f76de7d7f7aee5dc179fb2b91c84cd
andrewtran10 <andtran@umass.edu>
2022-04-29 23:51:49 -0400

    update secondaryView: only show HM if addons

--- frontend/src/containers/SecondaryView.js
+++ frontend/src/containers/SecondaryView.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Stack } from '@mui/material';
 import { Divider } from '@material-ui/core';
 
+import { SocketContext } from '../contexts/SocketContext';
 import UserInput from './SecondaryView/UserInput';
 import HealthMonitor from './SecondaryView/HealthMonitor';
 
@@ -9,12 +10,24 @@ const HealthMonitorMemo = React.memo(HealthMonitor);
 
 
 export default function SecondaryView() {
-  return (
+	const { holderAddons } = useContext(SocketContext);
+	const [availableAddons, setAvailableAddons] = useState([]);	
+
+	useEffect(() =>{
+		const interval = setInterval(()=>{
+			if (!(JSON.stringify(holderAddons.current) === JSON.stringify(availableAddons))) {
+				setAvailableAddons(holderAddons.current);
+			}
+		}, 100);
+		return () => clearInterval(interval);
+	});
+
+	return (
 	<React.Fragment>
 		<Stack alignItems='center' justifyContent='flex-start' spacing={3}>
 			<UserInput/>
 			<Divider flexItem style={{height: 5, width: '100%'}} />
-			<HealthMonitorMemo />
+			{ (availableAddons.length > 0) ? <HealthMonitorMemo /> : <div></div>}
 		</Stack>
 	</React.Fragment>
   )
